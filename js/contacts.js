7a31b4c57744aa303565fc121673be4a6f0e4087
KaleSG <jeremiahsuarez06@gmail.com>
2025-09-03 23:40:18 -0500

    contact animate

--- js/contacts.js
+++ js/contacts.js
@@ -1,9 +1,13 @@
 const contactItems = document.getElementsByClassName("contact-item");
-    
+const contactDivs = document.getElementsByClassName("contact-div");
+
     for (const item of contactItems) {
+
+       /* hide all individual contact items*/
+
         if (item.getAttribute("data-should-copy") == "false") continue;
         const copyUnder = item.querySelector(".clipboard-hold").querySelector(".copied-under");
-        console.log(copyUnder);
+ 
         item.addEventListener("pointerup",()=>{
             navigator.clipboard.writeText(item.getAttribute("value"))
             copyUnder.style.visibility = "visible";
@@ -13,8 +17,22 @@ const contactItems = document.getElementsByClassName("contact-item");
         })
     }
     
+
+
 /* contact tab show */
 function contactButton() {
+    for (const item of contactDivs) {
+        item.style.visibility = "hidden";
+        item.classList.add("contact-hidden");
+        item.classList.remove("contact-shown");
+    }
     document.getElementById("contact-list").style.display = "flex";
+    for (var i = 0; i < contactDivs.length; i++) {
+        const item = contactDivs[i];
+        setTimeout(() => {
+            item.style.visibility = "visible";
+            item.classList.remove("contact-hidden");
+            item.classList.add("contact-shown");
+    }, i * 100);
+    }
 }
-
